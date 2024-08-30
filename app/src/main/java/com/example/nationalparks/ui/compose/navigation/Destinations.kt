57e799696e3f80f5f8126bb0ce39e91e4c885263
dreadcrumb <11866363+dreadcrumb@users.noreply.github.com>
2024-08-30 20:52:03 +0200

    add destination logs

--- app/src/main/java/com/example/nationalparks/ui/compose/navigation/Destinations.kt
+++ app/src/main/java/com/example/nationalparks/ui/compose/navigation/Destinations.kt
@@ -1,5 +1,6 @@
 package com.example.nationalparks.ui.compose.navigation
 
+import android.util.Log
 import androidx.compose.runtime.Composable
 import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.navigation.NavHostController
@@ -13,9 +14,13 @@ import com.example.nationalparks.ui.viewmodels.TourListViewModelInterface
 
 @Composable
 fun TourListDestination(navController: NavHostController) {
+    Log.i("Destination", "TourListDestination")
     val viewModel: TourListViewModelInterface = hiltViewModel<TourListViewModel>()
     OrientationChangedComposable(
-        changedToLandscape = { navController.navigate("${NavigationKeys.Route.TOURS_LANDSCAPE}/${-1}") },
+        changedToLandscape = {
+            Log.i("Destination", "TourListDestination, changed to landscape")
+            navController.navigate("${NavigationKeys.Route.TOURS_LANDSCAPE}/${-1}")
+        },
         changedToPortrait = {}
     )
     TourListScreen(
@@ -28,9 +33,13 @@ fun TourListDestination(navController: NavHostController) {
 
 @Composable
 fun TourDetailDestination(navController: NavHostController) {
+    Log.i("Destination", "TourDetailDestination")
     val viewModel: TourDetailsViewModelInterface = hiltViewModel<TourDetailsViewModel>()
     OrientationChangedComposable(
-        changedToLandscape = { navController.navigate("${NavigationKeys.Route.TOURS_LANDSCAPE}/${viewModel.state.value.tour?.id ?: -1}") },
+        changedToLandscape = {
+            Log.i("Destination", "TourDetailDestination, changed to landscape")
+            navController.navigate("${NavigationKeys.Route.TOURS_LANDSCAPE}/${viewModel.state.value.tour?.id ?: -1}")
+        },
         changedToPortrait = {}
     )
     TourDetailsScreen(viewModel, true,
@@ -39,12 +48,14 @@ fun TourDetailDestination(navController: NavHostController) {
 
 @Composable
 fun TourListLandscapeDestination(navController: NavHostController) {
+    Log.i("Destination", "TourListLandscapeDestination")
     val listViewModel: TourListViewModelInterface = hiltViewModel<TourListViewModel>()
     val detailViewModel: TourDetailsViewModelInterface = hiltViewModel<TourDetailsViewModel>()
 
     OrientationChangedComposable(
         changedToLandscape = {},
         changedToPortrait = {
+            Log.i("Destination", "TourListLandscapeDestination, changed to portrait")
             val itemId = detailViewModel.state.value.tour?.id ?: -1
             if (itemId == -1) {
                 navController.navigate(NavigationKeys.Route.TOUR_LIST)
